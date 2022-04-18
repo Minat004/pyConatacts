c8cf802b1a61d0037411d0b8a2db58b35990d2ac
Minato <umi0n3@gmail.com>
2022-04-18 14:10:45 +0200

    layout for main window

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import PySimpleGUI as sg
 
-data_values = [[]]
-
-layout = [
-    [sg.Input(), sg.Button('Browse', key='-BROWSE-')],
-    [sg.Input(), sg.Combo(['Name', 'Surname', 'Number'], default_value='Name'), sg.Button('Search', key='-SEARCH-')],
-    [sg.Table(values=data_values)],
-    [sg.Button('Add')],
-    [sg.Button('Edit')],
-    [sg.Button('Delete')],
+data_values = [
+    ['Alex', 'Bell', 578364303],
+    ['Masha', 'Bell', 578364304],
 ]
+header = ['qwerty', 'asdfgh', 'zxcvbn']
 
-sg.Window('Phone Book', layout).read()
+
+def create_main_window():
+    layout = [
+        [sg.Input(), sg.Button('Browse', key='-BROWSE-', expand_x=True)],
+        [
+            sg.Input(),
+            sg.Combo(['Name', 'Surname', 'Number'], default_value='Name'),
+            sg.Button('Search', key='-SEARCH-')
+        ],
+        [sg.Table(values=data_values, headings=header, justification='left', expand_x=True)],
+        [
+            sg.Button('Add', key='-ADD-', expand_x=True),
+            sg.Button('Edit', key='-EDIT-', expand_x=True),
+            sg.Button('Delete', key='-DELETE-', expand_x=True),
+         ],
+    ]
+
+    return sg.Window('Phone Book', layout)
+
+
+main_window = create_main_window()
+
+while True:
+    event, values = main_window.read()
+    if event == sg.WIN_CLOSED:
+        break
